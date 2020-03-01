867e77cd2c14581b6df467c90bd49ee5b81d2687
virus-404 <agapito1998@gmail.com>
2020-03-01 20:48:40 +0200

    Spring initialitzation

---
+++ src/main/java/com/example/uLearn/ULearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.uLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ULearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ULearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uLearn/ULearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ULearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
