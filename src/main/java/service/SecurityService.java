717a659c5886991b6bed171cf9c68247a8e07daa
momolollo <mohamedelamin.zhioua@community.unipa.it>
2020-03-10 12:01:57 +0200

    Seting up login/sign in backend

---
+++ src/main/java/service/SecurityService.java
@@ -0,0 +1,7 @@
+package service;
+
+public interface SecurityService {
+    String findLoggedInUsername();
+
+    void autoLogin(String username, String password);
+}
---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,9 @@
+package service;
+
+import model.User;
+
+public interface UserService {
+    void save(User user);
+
+    User findByUsername(String username);
+}
